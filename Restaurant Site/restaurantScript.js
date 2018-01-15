322460027e043352a2a30ace25a5ccb95331dc63
Jakeduerr <Jakeduerr@live.com>
2018-01-14 21:42:31 -0600

    Restaurant Project

---
+++ Restaurant Site/restaurantScript.js
@@ -0,0 +1,34 @@
+$("#myCarousel").carousel();
+
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+
+$(document).ready(function(){
+   
+    $("#myCarousel").carousel();
+    
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+    
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
